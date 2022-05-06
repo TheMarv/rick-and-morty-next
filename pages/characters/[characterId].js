f65878d07782e45f3533ac4f8a8eeeb35f628002
Marvin Steckel <themarvdoteu@gmail.com>
2022-05-06 11:38:35 +0200

    Removed unnecessary code

--- pages/characters/[characterId].js
+++ pages/characters/[characterId].js
@@ -63,30 +63,3 @@ export default function Character() {
     return <h1>Loading data...</h1>;
   }
 }
-
-export function fetchCharaterDetails(id = 10) {
-  fetch(`https://rickandmortyapi.com/api/character/${query.characterId}`)
-    .then((response) => response.json())
-    .then((data) => {
-      charDetailsSection.innerHTML = '';
-      const episodeNum = data.episode[0].split('/').pop();
-
-      // const charDetailsElem = document.createElement('div');
-      // charDetailsElem.className = 'character__details--frame';
-      // charDetailsElem.innerHTML = `
-      // <img
-      //       src="${data.image}"
-      //       alt="picture of ${data.name}"
-      //     />
-      //     <div class="character__details-text">
-      //       <h2>${data.name}</h2>
-      //       <h3>First appeared in episode ${episodeNum}</h3>
-      //       <h3>Gender: ${data.gender}</h3>
-      //       <h3>Species: ${data.species}</h3>
-      //       <h4>Origin: ${data.origin.name}</h4>
-      //       <p>${data.type}</p>
-      //       <p>Last seen in ${data.location.name}</p>
-      //       `;
-      // charDetailsSection.append(charDetailsElem);
-    });
-}
